e847660a951c4da1c1f37e215c085d33d1809580
Tamara Xavier <tamaragiliski96@gmail.com>
2025-01-09 20:48:52 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    name: "Camiseta",
+    price: 29.99,
+  },
+  {
+    name: "Calça Jeans",
+    price: 59.99,
+  },
+  {
+    name: "Sapatos",
+    price: 59.99,
+  },
+  {
+    name: "Vestido",
+    price: 89.99,
+  },
+  {
+    name: "Chapéu",
+    price: 19.99,
+  },
+];
+
